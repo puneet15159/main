da55649abe7547b5f914d1ec0f1a90b03ba2b179
puneet <puneet15159@iiitd.ac.in>
2020-06-02 12:31:40 +0530

    Calculate Emp wage

--- start.sh
+++ start.sh
@@ -1,12 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
 isPresent=1;
 randomCheck=$((RANDOM%2))
 
 if [[ $isPresent -eq $randomCheck ]]
 then
-	echo "Employee is present"
+	empRatePerHr=20
+	empHrs=8
+	salary=$(($empHrs*$empRatePerHr));
 else
-	echo "Employee is absent"
+	salary=0
 fi
 

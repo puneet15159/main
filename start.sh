1bf09dcaa889e5e8081452f05537ab6acbf246f7
puneet <puneet15159@iiitd.ac.in>
2020-06-02 12:25:16 +0530

    attendence check

--- start.sh
+++ start.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-echo "Welcome"
+isPresent=1;
+randomCheck=$((RANDOM%2))
 
-attendence=$(( RANDOM%10+1))
+if [[ $isPresent -eq $randomCheck ]]
+then
+	echo "Employee is present"
+else
+	echo "Employee is absent"
+fi
 
-echo "Employee $attendence is present"

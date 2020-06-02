f7403a3d2e415b7b76a2843077171c6e2d17bca1
puneet <puneet15159@iiitd.ac.in>
2020-06-02 14:01:00 +0530

    Welcome page

--- start.sh
+++ start.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
 
-isPresent=1;
-randomCheck=$((RANDOM%2))
-
-if [[ $isPresent -eq $randomCheck ]]
-then
-	echo "Employee is present"
-else
-	echo "Employee is absent"
-fi
+echo "Welcome"
 

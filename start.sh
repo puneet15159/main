ea55164fc682cf20563c116d31d03cf438a91911
puneet <puneet15159@iiitd.ac.in>
2020-06-02 13:31:53 +0530

    Wage with function

--- start.sh
+++ start.sh
@@ -10,25 +10,34 @@ numberOfWorkingDays=20
 totalEmpHrs=0
 totalWorkingDays=0
 
+function getWorkingHrs(){
+empCheck=$((RANDOM%3))
+
+case $1 in
+        $isFullTime)
+                empHrs=8
+                        ;;
+        $isPartTime)
+                empHrs=4
+                        ;;
+        *)
+                empHrs=0
+                        ;;
+esac
+
+echo $empHrs
+
+}
+
 while [[ $totalEmpHrs -lt $maxHrsInMonth && $totalWorkingDays -lt $numberOfWorkingDays ]]
 do
 
 ((totalWorkingDays++))
 empCheck=$((RANDOM%3))
 
-case $empCheck in
-	$isFullTime)
-		empHrs=8
-			;;
-	$isPartTime)
-		empHrs=4
-			;;
-	*)
-		empHrs=0
-			;;
-esac
+workingHrs=`expr $getWorkingHrs $empCheck`
 
-totalEmpHrs=$(($empHrs+$totalEmpHrs));
+totalEmpHrs=$(($workingHrs+$totalEmpHrs));
 
 done
 
